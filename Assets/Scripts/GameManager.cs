c5f91bf81bea86544ed063d2085a9462121ae67e
Daniel Fimaini <daniel.fimiani@mercadolibre.com>
2025-05-19 21:27:14 -0300

    Add main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    private static GameManager instance;
+    public static GameManager Instance {
+        get {
+            if (instance == null) {
+                instance = FindObjectOfType<GameManager>();
+                if (instance == null) {
+                    GameObject obj = new GameObject("GameManager");
+                    instance = obj.AddComponent<GameManager>();
+                }
+            }
+            return instance;
+        }
+    }
+
     // Start is called before the first frame update
     void Start() {
 
@@ -9,5 +23,13 @@ public class GameManager : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
+    }
+
+    public void PauseGame() {
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame() {
+        Time.timeScale = 1f;
     }
 }
---
+++ Assets/Scripts/UI/MainPanel.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainPanel : MonoBehaviour {
+
+    [Header("UI Elements")]
+    [SerializeField] Button playButton;
+    [SerializeField] Button backButton;
+    [SerializeField] Button exitButton;
+
+    void Awake() {
+        playButton.onClick.AddListener(OnPlayButtonClicked);
+        exitButton.onClick.AddListener(OnExitButtonClicked);
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "MainMenu") {
+            backButton.onClick.AddListener(OnBackButtonClicked);
+            backButton.gameObject.SetActive(false);
+        }
+        else {
+            backButton.gameObject.SetActive(true);
+            exitButton.gameObject.SetActive(false);
+        }
+    }
+
+    void OnDestroy() {
+        playButton.onClick.RemoveListener(OnPlayButtonClicked);
+        exitButton.onClick.RemoveListener(OnExitButtonClicked);
+    }
+
+    private void OnPlayButtonClicked() {
+        // Check if where are in te game scene
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Level1")
+            GameManager.Instance.ResumeGame();
+
+        // Load the game scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+    }
+
+    private void OnBackButtonClicked() {
+        // Check if where are in te game scene
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Level1")
+            GameManager.Instance.PauseGame();
+
+        // Load the main menu scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+
+    private void OnExitButtonClicked() {
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+        Debug.Log("Exiting game");
+#endif
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+        Application.Quit();
+#endif
+    }
+}
