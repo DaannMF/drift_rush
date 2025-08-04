54c82469a265dfad2bc38d18c7b00d1e9f136347
Daniel Fimiani <recayte93@gmail.com>
2025-08-04 06:50:22 -0300

    add discord integrations

--- Assets/Scripts/Managers/DiscordManager.cs
+++ Assets/Scripts/Managers/DiscordManager.cs
@@ -1,6 +1,35 @@
 using UnityEngine;
 
 public class DiscordManager : MonoBehaviour {
+#if UNITY_STANDALONE_WIN
+    [SerializeField] long clientId;
+    private
+    Discord.Discord discord;
 
+    private void Start() {
+        discord = new Discord.Discord(clientId, (ulong)Discord.CreateFlags.NoRequireDiscord);
+        ChangeActivity();
+    }
 
+    void Update() {
+        discord?.RunCallbacks();
+    }
+
+    void OnApplicationQuit() {
+        discord?.Dispose();
+    }
+
+    private void ChangeActivity() {
+        var activityManager = discord.GetActivityManager();
+        var activity = new Discord.Activity {
+            State = "Drifting"
+        };
+
+        activityManager.UpdateActivity(activity, (result) => {
+            if (result == Discord.Result.Ok) {
+                Debug.Log("Discord Activity Updated Successfully");
+            }
+        });
+    }
+#endif
 }
